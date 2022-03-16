f52a362fc2b5166614aab469c0d07e4c241e8080
Fabricio <fabriciorateni@gmail.com>
2022-03-16 16:26:05 -0300

    display one

--- front/src/Game.tsx
+++ front/src/Game.tsx
@@ -13,7 +13,7 @@ type BoardState = {
 type Row = {
   id: number;
   cards: Card[];
-}
+};
 
 const initialState: BoardState = {
   cards: [
@@ -36,29 +36,60 @@ const initialState: BoardState = {
   ],
 };
 
-type Action = { type: "hide_all" } | { type: "display_one" };
+type Action =
+  | { type: "hide_all" }
+  | { type: "display_one"; selectedCardId: number };
 
 function memoryGameReducer(state: BoardState, action: Action): BoardState {
-  throw new Error("Function not implemented.");
+  switch (action.type) {
+    case "hide_all":
+      throw new Error("Function not implemented.");
+    case "display_one":
+      return {
+        ...state,
+        cards: state.cards.map((card) =>
+          card.id === action.selectedCardId
+            ? { ...card, isDisplayed: true }
+            : card
+        ),
+      };
+  }
 }
 
 function getRows(squares: Card[]): Row[] {
   let rows: Row[] = [];
-  for (let i=0; i < squares.length; i = i + 4) {    
+  for (let i = 0; i < squares.length; i = i + 4) {
     const cellsInRow = squares.slice(i, i + 4);
-    rows.push({ id: i, cards: cellsInRow});
-  };
+    rows.push({ id: i, cards: cellsInRow });
+  }
   return rows;
 }
 
 type RowViewProps = {
-  row: Row
+  row: Row;
+  dispatch: React.Dispatch<Action>;
+};
+
+interface CustomEvent extends React.MouseEvent<HTMLSpanElement> {
+  card: Card;
 }
 
-const handleClick = (e: React.MouseEvent<HTMLSpanElement>) : void => console.log(e);
-
 function RowView(props: RowViewProps) {
-  return (<tr>{props.row.cards.map(card => <td key={card.id}> <span onClick={handleClick}>{card.isDisplayed ? card.emoji : '⬜'}</span> </td>)}</tr>)
+
+  const handleClick = (e: CustomEvent): void =>
+    props.dispatch({ type: "display_one", selectedCardId: e.card.id });
+
+  return (
+    <tr>
+      {props.row.cards.map((card) => (
+        <td key={card.id}>
+          <span onClick={(e) => handleClick({ ...e, card })}>
+            {card.isDisplayed ? card.emoji : "⬜"}
+          </span>
+        </td>
+      ))}
+    </tr>
+  );
 }
 
 export function Game() {
@@ -66,11 +97,13 @@ export function Game() {
 
   const rows = getRows(state.cards);
 
-  return (  
-    <div>    
+  return (
+    <div>
       <table style={{ border: "solid" }}>
         <tbody>
-          {rows.map((r) => <RowView key={r.id} row={r} />)}
+          {rows.map((r) => (
+            <RowView key={r.id} row={r} dispatch={dispatch} />
+          ))}
         </tbody>
       </table>
     </div>
