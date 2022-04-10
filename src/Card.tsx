840867beba44928d02bef2d0c8d850f985e00fb4
Fabricio <fabriciorateni@gmail.com>
2022-04-10 11:43:38 -0300

    flip card effect

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,3 +1,4 @@
+import ReactCardFlip from "react-card-flip";
 import { CardData } from "./gameReducer";
 
 type CardProps = {
@@ -7,18 +8,26 @@ type CardProps = {
 };
 export function Card(props: CardProps) {
   const oneBasedId = props.cardData.id + 1;
+  const isFlipped = props.cardData.displayedBy !== null;
 
   return (
     <div className="transition ease-in-out delay-70 hover:-translate-y-1 hover:scale-105 duration-100 hover:bg-mint text-lg border border-slate-300">
-      <button
-        aria-label={`card-${oneBasedId}`}
-        title={`Card ${oneBasedId}`}
-        className={`${props.canClickCard ? "cursor-pointer" : "cursor-not-allowed"}`}
-        disabled={!props.canClickCard}
-        onClick={props.onClick}
-      >
-        {props.cardData.displayedBy ? props.cardData.emoji : "⬜"}
-      </button>
+      <ReactCardFlip isFlipped={isFlipped}>
+        <div>
+          <button
+            aria-label={`card-${oneBasedId}`}
+            title={`Card ${oneBasedId}`}
+            className={`${
+              props.canClickCard ? "cursor-pointer" : "cursor-not-allowed"
+            }`}
+            disabled={!props.canClickCard}
+            onClick={props.onClick}
+          >
+            ⬜
+          </button>
+        </div>
+        <div>{props.cardData.emoji}</div>
+      </ReactCardFlip>
     </div>
   );
 }
--- src/test/Game.test.tsx
+++ src/test/Game.test.tsx
@@ -3,13 +3,18 @@ import { render, screen } from '@testing-library/react';
 import { Game } from '../Game';
 import userEvent from '@testing-library/user-event';
 
-test('clicking on card flips it', () => {
+test('clicking on card flips it', async () => {
   render(<Game />);
   const card = screen.getByRole('button',{ name:  /card-11/i});
 
   expect(card).toHaveTextContent('⬜');
 
-  userEvent.click(card);  
+  userEvent.click(card);
 
-  expect(card).not.toHaveTextContent('⬜');
+  /* Because of the way react-card-flip handles button visibilty
+     once clicked we cannot test the elemenet being removed (waitForElementToBeRemoved) 
+     or expect(card).not.toVisible() in this case.
+     Falling back to using the disable atribute although it is not ideal.
+  */   
+  expect(card).toHaveAttribute('disabled');
 });
